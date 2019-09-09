d3e529a2c2868eedbc7c87c75a7678785b6c1f49
Dev Ryu <eostmghf01@gmail.com>
2019-09-09 21:48:26 +0900

    3-5
    상품등록하기1

--- product/views.py
+++ product/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.edit import FormView  # 폼뷰를 가져온다
 from .models import Product
+from .forms import RegisterForm
 
 
 class ProductList(ListView):
@@ -8,3 +10,9 @@ class ProductList(ListView):
     template_name = 'product.html'
     context_object_name = 'product_list'
 # 포문에 기본 변수가 오브젝트리스트, 싫으면  views에서 context_object_name 설정해야함
+
+
+class ProductCreate(FormView):
+    template_name = 'register_product.html'
+    form_class = RegisterForm
+    success_url = '/product/'
--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from user.views import index, RegisterView, LoginView
-from product.views import ProductList
+from product.views import ProductList, ProductCreate
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
@@ -24,4 +24,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
     path('product/', ProductList.as_view()),
+    path('product/create/', ProductCreate.as_view()),
+
 ]
